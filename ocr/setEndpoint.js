a806047775b8296ce2b699c6ddbf157a810a074f
ikaros0909 <ikaros0909@gmail.com>
2021-01-13 11:59:19 +0900

    test

--- ocr/setEndpoint.js
+++ ocr/setEndpoint.js
@@ -1,5 +1,6 @@
 // $env:GOOGLE_APPLICATION_CREDENTIALS="D:\GitHub\ocr_googleapi_test\auth\StudyEnglish-0e5094e14cd7.json"
-
+const fs = require("fs");
+const path = require("path")
 const express = require("express");
 const router = express.Router();
 
@@ -14,7 +15,22 @@ async function setEndpoint() {
   const client = new vision.ImageAnnotatorClient(clientOptions);
 
   // Performs text detection on the image file
-  const [result] = await client.textDetection('./resources/test.png');
+  // const [result] = await client.textDetection('./resources/test2.png');
+  let text;
+  const fileName = "./resources/test5_1.png";
+  const imageFile = fs.readFileSync(fileName);
+  const image = Buffer.from(imageFile).toString("base64");
+  const request = {
+    image: {
+      content: image
+    },
+    imageContext: {
+      languageHints: ["ko"]
+    }
+  };
+  
+  const [result] = await client.textDetection(request);
+
   const labels = result.textAnnotations;
   console.log('Text:');
   labels.forEach(label => console.log(label.description));
